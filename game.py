b976703a6b08aaca3a12e9c34ba27979c4c53a5b
Jarod <jpiv12307@gmail.com>
2020-05-25 15:07:54 -0500

    asdfsdf

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-input("death")
+import pygame, random, sys
+
+pygame.init()
+disp = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
+disp.fill((255,255,255))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: pygame.quit(); sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE: pygame.quit(); sys.exit()
+    pygame.display.flip()
